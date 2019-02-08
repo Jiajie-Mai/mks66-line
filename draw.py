8f246069be24b49d6510dbc19a7c135248b3be6f
Jiajie-Mai <jmai3@stuy.edu>
2019-02-08 12:34:22 -0500

    did things

--- draw.py
+++ draw.py
@@ -21,9 +21,9 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     elif (m <= 1 and m >= 0):
         first( x0, y0, x1, y1, a, b, screen, color )
     elif(m < 0 and m >= -1):
-        seventh( x0, y0, x1, y1, a, b, screen, color )
+	eighth( x0, y0, x1, y1, a, b, screen, color )	
     elif (m < -1):
-        eighth( x0, y0, x1, y1, a, b, screen, color )
+        seventh( x0, y0, x1, y1, a, b, screen, color )
 
 #this is for the fist octant
 def first( x0, y0, x1, y1, a, b, screen, color ):
@@ -37,7 +37,6 @@ def first( x0, y0, x1, y1, a, b, screen, color ):
             d += 2 * b
         x += 1
         d += 2 * a
-    return 1
 
 #this is for the second octant
 def second(  x0, y0, x1, y1, a, b, screen, color ):
@@ -51,32 +50,29 @@ def second(  x0, y0, x1, y1, a, b, screen, color ):
             d += 2 * a
         y += 1
         d += 2 * b
-    return 1
 
-#this is for the eighth octant
-def eighth( x0 , y0, x1, y1, a, b, screen, color ):
+#this is for the seventh octant
+def seventh( x0 , y0, x1, y1, a, b, screen, color ):
     x = x0
     y = y0
-    d = 2 * b - a
+    d = 2 * b + a
     while y > y1:
         plot(  screen, color, x , y )
         if d > 0:
             x += 1
-            d -= 2 * a
+            d += 2 * a
         y -= 1
         d -= 2 * b
-    return 1
 
-#this is for the seventh octant
-def seventh(  x0 , y0, x1, y1, a, b, screen, color ):
+#this is for the eighth octant
+def eighth(  x0 , y0, x1, y1, a, b, screen, color ):
     x = x0
     y = y0
-    d = 2 * a - b
+    d = 2 * a + b
     while y < y1:
         plot(  screen, color, x , y )
         if d < 0:
             y -= 1
             d -= 2 * b
-        a += 1
+        x += 1
         d += 2 * a
-    return 1
