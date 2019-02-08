9183828763f13945149d1f1c0fa89bffeda98ec9
Jiajie-Mai <jmai3@stuy.edu>
2019-02-08 15:36:46 -0500

    found problem

--- draw.py
+++ draw.py
@@ -4,7 +4,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     #calculates slope
     #changes to second octant if the slope is undefined and goes up
     #changes to seventh octant if the slope is undefined and goes down
-    if (x1 - x0 == 0 and y1 - y0 <= 0):
+    if (x1 - x0 == 0 and y1 - y0 < 0):
         m = -2
     elif (x1 - x0 == 0 and y1 - y0 > 0):
         m = 2
@@ -17,12 +17,16 @@ def draw_line( x0, y0, x1, y1, screen, color ):
 
     #does things depending on slope
     if  (m > 1):
+	print("Doing second octant") 
         second( x0, y0, x1, y1, a, b, screen, color )
     elif (m <= 1 and m >= 0):
+	print("Doing first octant")
         first( x0, y0, x1, y1, a, b, screen, color )
     elif(m < 0 and m >= -1):
+	print("Doing eighth octant")
 	eighth( x0, y0, x1, y1, a, b, screen, color )	
     elif (m < -1):
+	print("Doing seventh octant")
         seventh( x0, y0, x1, y1, a, b, screen, color )
 
 #this is for the fist octant
@@ -55,22 +59,24 @@ def second(  x0, y0, x1, y1, a, b, screen, color ):
 def seventh( x0 , y0, x1, y1, a, b, screen, color ):
     x = x0
     y = y0
-    d = 2 * b + a
-    while y > y1:
-        plot(  screen, color, x , y )
+    d = 2 * a + b
+    print(y)
+    print(y1)
+    while y <= y1:
+        plot( screen, color, x , y )
         if d > 0:
             x += 1
             d += 2 * a
         y -= 1
-        d -= 2 * b
+        d -= 2 * a
 
 #this is for the eighth octant
-def eighth(  x0 , y0, x1, y1, a, b, screen, color ):
+def eighth( x0 , y0, x1, y1, a, b, screen, color ):
     x = x0
     y = y0
-    d = 2 * a + b
-    while y < y1:
-        plot(  screen, color, x , y )
+    d = 2 * b + a
+    while x <= x1:
+        plot( screen, color, x , y )
         if d < 0:
             y -= 1
             d -= 2 * b
--- main.py
+++ main.py
@@ -4,16 +4,31 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-#0 slope
-draw_line(250, 250, 500, 250, screen, color)
-#undefined slope (up)
-draw_line(250, 250, 250, 500, screen, color)
-#undefined slope (down)
-draw_line(250, 250, 250, -500, screen, color)
-#1 slope
-draw_line(250, 250, 500, 500, screen, color)
-#-1 slope
-draw_line(250, 250, 500, -500, screen, color)
+# 0 slope
+#draw_line(250, 250, 500, 250, screen, color)
+# undefined slope (up)
+#draw_line(250, 250, 250, 500, screen, color)
+
+# undefined slope (down)
+draw_line(250, 250, 250, 0, screen, color)
+
+# 1 slope (45 degrees)
+#draw_line(250, 250, 500, 500, screen, color)
+
+# -1 slope (315 degrees)
+#draw_line(250, 250, 500, 0, screen, color)
+
+# 1.5 slope (67.5 degrees)
+#draw_line(250, 250, 375, 500, screen, color)
+
+# -1.5 slope (-292.5 degrees)
+#draw_line(250, 250, 375, 0, screen, color)
+
+# 0.5 slope (22.5 degrees)
+#draw_line(250, 250, 500, 375, screen, color)
+
+# -0.5 slope (337.5 degrees)
+#draw_line(250, 250, 500, 125, screen, color)
 
 
 
